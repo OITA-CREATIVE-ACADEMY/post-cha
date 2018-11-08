bb5219d8ce680e98ecf149edb11f3cb1a3571758
yukikatsuragi <yuki@hab-co.jp>
2018-11-08 20:30:55 +0900

    ログイン機能の追加

--- public/js/app.js
+++ public/js/app.js
@@ -7,39 +7,47 @@ var config = {
   storageBucket: "post-cha.appspot.com",
   messagingSenderId: "656801433168"
 };
+
 firebase.initializeApp(config);
 // 認証状態の確認
 firebase.auth().onAuthStateChanged(function(user) {
     if(user) {
-    
         // ログイン状態の処理
         alert("いまログイン中！");
-    
+
     }
     else {
-    
         // ログアウト状態の処理
         alert("いまはログインしてません！！");
     }
 });
+
 window.onload = function () {
 
   var button;
 
   // ボタンがクリックされた時の処理
-  $("#newuser, #login").on('click', function (e) {
+  $(".input_signup, .input_login").on('click', function (e) {
+    // alert("押したよ！")
 
     // メールアドレス・パスワードを取得
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
+    var email = $(".input_email").val();
+    var password = $(".input_password").val();
+    var userName = $(".input_nickName").val();
 
     // 新規ユーザーを登録
     firebase.auth().createUserWithEmailAndPassword(email, password)
+    // .catch(function(error) {
+    //   alert('登録できません（' + error.message + '）');
+    // });
+    // ログイン
+    firebase.auth().signInWithEmailAndPassword(email, password)
 
   });
-  $("#logout").on('click', function (e) {
+  $(".logout").on('click', function (e) {
 
     firebase.auth().signOut();
+    alert("ログアウトしました！");
 
   });
 };
