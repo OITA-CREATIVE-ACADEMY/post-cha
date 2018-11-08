f4ad4489a0d137ac7d1a92a8d3f4c8ee8f1df18d
OCA <mitsune.kuu2@gmail.com>
2018-11-08 18:51:29 +0900

    11/8 wip html/css/jsを少し書きました

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,21 @@
 // POST-cha!のjsファイルです
+
+$(function(){
+
+
+    //ログイン・新規登録ボタンを押した時、モーダルを開く
+    $(document).on('click', '.modal', modalShow);
+    
+    function modalShow(e){
+        var target = $(e.target);
+        console.log(target);
+        if (target.hasClass('add-modal')) {
+
+        
+    }
+
+
+
+});
+
+
