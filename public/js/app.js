b889e13b9af615f2c53ed3902071e60b6466e81f
yukikatsuragi <yuki@hab-co.jp>
2018-11-13 20:39:48 +0900

    ログイン周りの修正

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 // Initialize Firebase
-var config = {
+const config = {
   apiKey: "AIzaSyAASntGfp-FPAUHu-5II9y3s8pI0XppIZI",
   authDomain: "post-cha.firebaseapp.com",
   databaseURL: "https://post-cha.firebaseio.com",
@@ -9,45 +9,60 @@ var config = {
 };
 
 firebase.initializeApp(config);
+
 // 認証状態の確認
 firebase.auth().onAuthStateChanged(function(user) {
     if(user) {
         // ログイン状態の処理
-        alert("いまログイン中！");
-
+        alert("いまログイン中!!!");
+        $("#signup, #login").hide();
+        console.log(user);
     }
     else {
         // ログアウト状態の処理
         alert("いまはログインしてません！！");
+        $("#signup, #login").show();
+        $("#logout").hide();
     }
 });
 
 window.onload = function () {
 
-  var button;
-
   // ボタンがクリックされた時の処理
-  $(".input_signup, .input_login").on('click', function (e) {
-    // alert("押したよ！")
+    $("#js-signUp").on('click', function (e) {
+        signUp()
+    });
 
-    // メールアドレス・パスワードを取得
-    var email = $(".input_email").val();
-    var password = $(".input_password").val();
-    var userName = $(".input_nickName").val();
+    $("#js-signIn").on('click', function (e) {
+        signIn()
+    });
 
-    // 新規ユーザーを登録
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-    // .catch(function(error) {
-    //   alert('登録できません（' + error.message + '）');
-    // });
-    // ログイン
-    firebase.auth().signInWithEmailAndPassword(email, password)
-
-  });
-  $(".logout").on('click', function (e) {
-
-    firebase.auth().signOut();
-    alert("ログアウトしました！");
-
-  });
+    $("#js-signOut").on('click', function (e) {
+        signOut()
+    });
 };
+
+function signUp() {
+  var email = $("#js-email").val();
+  var password = $("#js-password").val();
+
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+      .catch(function(error) {
+          console.log(error);
+      });
+}
+
+function signIn() {
+    var email = $("#js-email").val();
+    var password = $("#js-password").val();
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
+function signOut() {
+    firebase.auth().signOut();
+    alert("ログアウトしました");
+}
