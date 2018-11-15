30a89e657d0a98be1ada01799dcdfe6cf54083f4
yukikatsuragi <yuki@hab-co.jp>
2018-11-15 20:10:52 +0900

    app.jsをmain.jsに変更 , script.jsを削除

---
+++ public/js/main.js
@@ -0,0 +1,161 @@
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyAASntGfp-FPAUHu-5II9y3s8pI0XppIZI",
+  authDomain: "post-cha.firebaseapp.com",
+  databaseURL: "https://post-cha.firebaseio.com",
+  projectId: "post-cha",
+  storageBucket: "post-cha.appspot.com",
+  messagingSenderId: "656801433168"
+};
+
+firebase.initializeApp(config);
+
+// 認証状態の確認
+firebase.auth().onAuthStateChanged(function(user) {
+    if(user) {
+        // ログイン状態の処理
+        $(".js-signed-in").show();
+        $(".js-signed-out").hide();
+
+        //ヘッダーにユーザーのメールアドレスを追加
+        $("#js-user-email").text(user.email);
+
+        console.log(user);
+    }
+    else {
+        // ログアウト状態の処理
+        //ヘッダーにユーザーのメールアドレスを追加
+        $("#js-user-email").text("");
+
+        alert("いまはログインしてません！！");
+        $(".js-signed-in").show();
+        $(".js-signed-out").hide();
+    }
+});
+
+window.onload = function () {
+
+  // ボタンがクリックされた時の処理
+    $("#js-signUp").on('click', function (e) {
+        signUp()
+    });
+
+    $("#js-signIn").on('click', function (e) {
+        signIn()
+    });
+
+    $("#js-signOut").on('click', function (e) {
+      signOut()
+    });
+};
+
+function signUp() {
+  var email = $("#js-email").val();
+  var password = $("#js-password").val();
+
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+      .catch(function(error) {
+          console.log(error);
+      });
+}
+
+function signIn() {
+    var email = $("#js-email").val();
+    var password = $("#js-password").val();
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
+function signOut() {
+    firebase.auth().signOut();
+    alert("ログアウトしました");
+
+    $("#js-signOut").on('click', function (e) {
+      signOut()
+    });
+};
+
+
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyAASntGfp-FPAUHu-5II9y3s8pI0XppIZI",
+  authDomain: "post-cha.firebaseapp.com",
+  databaseURL: "https://post-cha.firebaseio.com",
+  projectId: "post-cha",
+  storageBucket: "post-cha.appspot.com",
+  messagingSenderId: "656801433168"
+};
+
+firebase.initializeApp(config);
+
+// 認証状態の確認
+firebase.auth().onAuthStateChanged(function(user) {
+    if(user) {
+        // ログイン状態の処理
+        $("#signup, #login").hide();
+
+        //ヘッダーにユーザーのメールアドレスを追加
+        $("#js-user-email").text(user.email);
+
+        console.log(user);
+    }
+    else {
+        // ログアウト状態の処理
+        //ヘッダーにユーザーのメールアドレスを追加
+        $("#js-user-email").text("");
+
+        alert("いまはログインしてません！！");
+        $("#signup, #login").show();
+        $("#logout").hide();
+    }
+});
+
+window.onload = function () {
+
+  // ボタンがクリックされた時の処理
+    $("#js-signUp").on('click', function (e) {
+        signUp()
+    });
+
+    $("#js-signIn").on('click', function (e) {
+        signIn()
+    });
+
+    $("#js-signOut").on('click', function (e) {
+      signOut()
+    });
+};
+
+function signUp() {
+  var email = $("#js-email").val();
+  var password = $("#js-password").val();
+
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+      .catch(function(error) {
+          console.log(error);
+      });
+}
+
+function signIn() {
+    var email = $("#js-email").val();
+    var password = $("#js-password").val();
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
+function signOut() {
+    firebase.auth().signOut();
+    alert("ログアウトしました");
+
+    $("#js-signOut").on('click', function (e) {
+      signOut()
+    });
+};
+
+
