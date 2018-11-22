9305a45b878dc1b54e3ab99c167e7fbbff03907d
OCA <mitsune.kuu2@gmail.com>
2018-11-22 19:13:35 +0900

    11/22 nicoのimport失敗しました

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import BootstrapVue from "bootstrap-vue"
 import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
+// nicoをインポート
+import "../node_modules/bootstrap-vue/dist/js/nico.bootstrap.bundle.min.js"
+import "../node_modules/bootstrap-vue/dist/js/nico.bootstrap.min.js"
+import "../node_modules/bootstrap/dist/css/nico.bootstrap.min.css"
 
 Vue.use(BootstrapVue)
 
